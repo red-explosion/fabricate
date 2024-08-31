8a6d8595d1685f83a025f04c6ca7d8eb1cc71f44
Ben Sherred <ben@sherred.co.uk>
2024-08-31 11:03:01 +0100

    fix: removing default controller

--- src/Modules/Default/Tasks/RemoveDefaultController.php
+++ src/Modules/Default/Tasks/RemoveDefaultController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace RedExplosion\Fabricate\Modules\Default\Tasks;
 
 use Illuminate\Filesystem\Filesystem;
+use RedExplosion\Fabricate\Actions\ReplaceInFileAction;
 use RedExplosion\Fabricate\Data\InstallData;
 use RedExplosion\Fabricate\Task;
 
@@ -12,6 +13,7 @@ class RemoveDefaultController extends Task
 {
     public function __construct(
         protected readonly Filesystem $filesystem,
+        protected readonly ReplaceInFileAction $replaceInFile,
     ) {
     }
 
@@ -22,6 +24,22 @@ class RemoveDefaultController extends Task
 
     public function perform(InstallData $data): void
     {
+        $controllers = $this->filesystem->allFiles(app_path('Http/Controllers'));
+
+        foreach ($controllers as $controller) {
+            $this->replaceInFile->handle(
+                'use App\Http\Controllers\Controller;',
+                '',
+                $controller->getPathname(),
+            );
+
+            $this->replaceInFile->handle(
+                ' extends Controller',
+                '',
+                $controller->getPathname(),
+            );
+        }
+
         $this->filesystem->delete(base_path('app/Http/Controllers/Controller.php'));
 
         $this->filesystem->put(base_path('app/Http/Controllers/.gitkeep'), '');
